4814b571e47f07a8e503fc2e85467b1712bb9824
zlfzlfgmlwns <zlfzlfgmlwns@naver.com>
2019-02-23 13:18:56 +0900

    webpack bable setting

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const title = '안녕하세요1212';
+ 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
+ 
+
+
+module.hot.accept();
